6209358b53ea2ba8a62714a9804619c8c1ae89b4
Taylor Beseda <tbeseda@gmail.com>
2015-10-08 22:07:35 -0600

    stringify the JSON response - because javascript

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 module.exports = function(context, req, res) {
   var theGoods = {
-    "frames": [
+    frames: [
       {
-        "index": 0,
-        "text": "spooky",
-        "icon": "a77"
-      }
-    ]
-  }
+        index: 0,
+        text: 'spooky',
+        icon: 'a77',
+      },
+    ],
+  };
   res.writeHead(200, { 'Content-Type': 'application/javascript' });
-  res.end(theGoods);
-}
+  res.end(JSON.stringify(theGoods));
+};
