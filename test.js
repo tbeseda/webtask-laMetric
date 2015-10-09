f944d1fb0a9a2aa52d01d3c9ec486eeddfdfd86f
Taylor Beseda <tbeseda@gmail.com>
2015-10-08 22:22:40 -0600

    use time instead of spooky - keep the ghost tho

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 module.exports = function(context, req, res) {
+  var t = new Date();
   var theGoods = {
     frames: [
       {
         index: 0,
-        text: 'spooky',
+        text: t.getHours() + ':' + t.getMinutes() + ':' + t.getSeconds(),
         icon: 'a77',
       },
     ],
